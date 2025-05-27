31f2a3ca9a7102bdb7a445b73e0a625f2c868b86
HarendraRathnayaka <127397899+HarendraRathnayaka@users.noreply.github.com>
2025-05-27 11:11:55 +0530

    fix: update base path of Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/SolarSense/',
+  base: '/solarsense/',
   build: {
     outDir: "dist",
   },
